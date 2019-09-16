962e000b72097ebea942745977334f570ae3e258
Muhammad Asif Javed <axif.javed@gmail.com>
2019-09-16 23:48:58 +0500

    issue script created

---
+++ exec_console_log_stack_issue.js
@@ -0,0 +1,18 @@
+console.log('i am from the top line -- just for debugging -- need to be ignore in final stdout');
+
+function sum(x, y) {
+	return x + y;
+}
+
+console.log(sum(20, 30));
+
+//as final output it should print 50
+//but it always print
+/**
+ *
+ * `i am from the top line -- just for debugging -- need to be ignore in final stdout
+ * 50
+ * `
+ */
+
+
