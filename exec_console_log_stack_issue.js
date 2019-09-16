d3dd2b0b4a69ae75e806bdaf3894d2b1de965b3d
Muhammad Asif Javed <axif.javed@gmail.com>
2019-09-17 00:49:30 +0500

    stdout live working but show last two console.log as consective(as same string)

--- exec_console_log_stack_issue.js
+++ exec_console_log_stack_issue.js
@@ -3,7 +3,27 @@ console.log('i am from the top line -- just for debugging -- need to be ignore i
 function sum(x, y) {
 	return x + y;
 }
-console.log(sum(20, 30));
+const result = sum(20, 30)
+console.log(result);
+
+
+/**
+ * final output should be send as stringify
+ * so i can distinguish that i have to ignore this stdout or have to parse the
+ * data
+ */
+
+const output = {
+	message: 'success',
+	json: true,
+	data: {
+		result: result
+	}
+}
+
+console.log(JSON.stringify(output))
+
+
 
 //as final output it should print 50
 //but it always print
