bc0b013587cd3184c14abf0ac084b77ca6e8c822
Muhammad Asif Javed <axif.javed@gmail.com>
2019-09-17 00:24:09 +0500

    added pipe to the stdout and stderr and exit events

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var exec = require('child_process').exec;
+const { exec } = require('child_process')
 
-var command = `node ./exec_console_log_stack_issue.js`;
-//function execute(command, callback){
-    exec(command, function(error, stdout, stderr){ console.log(stdout); });
-//};
+const command = 'node ./exec_console_log_stack_issue.js'
+const process = exec(command)
 
+  process.stdout.on('data', (data) => {
+    console.log('stdout: ' + data.toString())
+  })
+
+  process.stderr.on('data', (data) => {
+    console.log('stderr: ' + data.toString())
+  })
+
+  process.on('exit', (code) => {
+    console.log('child process exited with code ' + code.toString())
+  })
 
